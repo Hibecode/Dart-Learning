ce82f9c24ff3e82e105f7ea841ddbde3a627e486
Hibecode <ibkokunoye@gmail.com>
2022-02-07 17:44:07 +0100

    Fundamentals

---
+++ bin/Fundamentals.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+
+
+
+void main() {
+
+  int yams =445;
+
+///Documentation
+
+  stdout.writeln('What is your name? h');
+  var Snfn = stdin.readLineSync();
+  print('My name is - $Snfn');
+
+}
