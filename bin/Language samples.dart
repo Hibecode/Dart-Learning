9435f53ea90853e1875aa1f386ea28ad9996239b
Hibecode <ibkokunoye@gmail.com>
2022-02-08 23:52:36 +0100

    changes

--- bin/Language samples.dart
+++ bin/Language samples.dart
@@ -3,6 +3,11 @@
 void main() {
     var name = 'Ibukun';
     var year = 1059;
+
+
+
+
+
     var pi = 3.14;
     var listOfNames = ['Jupiter', 'Saturn', 'Uranus', 'Neptun'];
     var image = {
@@ -10,6 +15,38 @@ void main() {
       'url' : '//path/to/saturn.jpg'
     };
     print(listOfNames[0]);
+
+
+
+    dynamic chickens = 506;
+    print(chickens);
+    chickens = "SOLD!";
+    print(chickens);
+
+    var s4 = '''GGNGJN
+    NFJJF''';
+
+    var s5 = '''GGNGJN
+    NFJJF''';
+
+
+    print(s4);
+    print(s5);
+
+
+
+    
+  var s1 = 'How\'re you today?';
+  var s2 = "How're you today?";
+  var s3 = r"How're you today \n?";
+
+print('');
+print(s1);
+print(s2);
+
+
+
+
 }
 
 
---
+++ bin/Type_conversion.dart
@@ -0,0 +1,23 @@
+
+
+
+void main() {
+
+var two = int.parse('2');
+assert(2 == two);
+
+var day = 34.toString();
+assert('34' == day);
+
+String dayt = 34.345.toStringAsFixed(2);
+assert('34.34' == dayt);
+
+
+const pi = 3.142;
+print(pi);
+
+
+
+
+}
+
